e4a6a9e5c94da2d874ba7ea3fa55b295c9be817a
DorianDevUA <dorian.devua@gmail.com>
2023-12-02 18:20:37 +0400

    Додає скрол для мобільного меню та модального вікна

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -4,15 +4,15 @@
   const closeMenuBtn = document.querySelector('.js-close-mobile-menu');
 
   const toggleMenu = () => {
-    const isMenuOpen =
-      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
+    document.body.classList.toggle("mobile-menu-open");
 
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
+    // const scrollLockMethod = !isMenuOpen
+    //   ? 'disableBodyScroll'
+    //   : 'enableBodyScroll';
+    // bodyScrollLock[scrollLockMethod](document.body);
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
